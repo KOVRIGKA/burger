a2d8c978350bf9d974e4b527905ee25eeb6231d9
Kovrigina Anastasia <anastasiia.kovrighina@mail.ru>
2018-11-22 21:47:15 +0300

    тз 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 function mobileMenu(){
-    const navTrigger = document querySelector('.nav-trigger');
-    const trigger = document querySelector('.nav-trigger__link');
-    const navMobile = document querySelector('.nav-mobile');
-    const navItems = document querySelectorAll('.nav-mobile__item');
+    const navTrigger = document.querySelector('.nav-trigger');
+    const trigger = document.querySelector('.nav-trigger__link');
+    const navMobile = document.querySelector('.nav-mobile');
+    const navItems = document.querySelectorAll('.nav-mobile__item');
 
     trigger.addEventListener('click', toggleClass);
 
@@ -11,7 +11,7 @@ function mobileMenu(){
     }
 
     function toggleClass(e){
-        e.preveventDefault();
+        e.preventDefault();
         navTrigger.classList.toggle('is-active');
         navMobile.classList.toggle('is-active');
     }
