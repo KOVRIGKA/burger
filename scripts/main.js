ede4df77ec78db4ea6b8e2161c12a6c5b7ea9101
Kovrigina Anastasia <anastasiia.kovrighina@mail.ru>
2018-11-26 19:49:31 +0300

    Update 2018-11-26T16:49:24.854Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+// мобильное меню
+
 function mobileMenu(){
     const navTrigger = document.querySelector('.nav-trigger');
     const trigger = document.querySelector('.nav-trigger__link');
@@ -18,3 +20,57 @@ function mobileMenu(){
 }
 
 mobileMenu();
+
+//слайдер бургеров
+
+function mySlider() {
+    const next = document.querySelector('.scroll__btn--next');
+    const prev = document.querySelector('.scroll__btn--prev');
+    const list = document.querySelector('.slider__list');
+    const item = list.querySelectorAll('.slider__item');
+
+    let num = 2
+
+    next.addEventListener('click', moveNext);
+    prev.addEventListener('click', movePrev);
+
+    function moveNext(){
+        num++
+        if (num > item.length){
+            num = 1;
+        }
+        setOrder();
+        list.classList.remove('is-reverse');
+        moveItem();
+    }
+
+    function moveNext(){
+        num--
+        if (num === 0){
+            num = item.length;
+        }
+        setOrder();
+        list.classList.add('is-reverse');
+        moveItem();
+    }
+
+    function setOrder(){
+        let key = num
+        for(const i of items){
+            i.style.order = key
+            key++
+            if(key > item.length){
+                key = 1;
+            }
+        }
+    }
+
+    function moveItem(){
+        list.classList.remove('is-move');
+        setTimeout(() => {
+            list.classList.add('is-move');            
+        }, 50);
+    }
+}
+
+mySlider();
